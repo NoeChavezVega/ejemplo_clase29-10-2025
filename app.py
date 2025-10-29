502e2dc6ce74559107f0aa1640c0c78061f825ee
NoeChavezVega <a377347@uach.mx>
2025-10-29 09:25:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ st.title("Ejemplo para usar sesion_state")
 
 count = 0
 
-increment = st.button("Increment")
-if increment :
-  count += 1
   
 st.write("Count =" , count)
 # st. sessio_state= demuestra las cantidades que va guardando 
