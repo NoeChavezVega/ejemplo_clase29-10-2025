fa58362e450675d395bf1180205aee0a740bfe4b
NoeChavezVega <a377347@uach.mx>
2025-10-29 09:04:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ st.write("Count =" , count)
 # st. sessio_state= demuestra las cantidades que va guardando 
 st.session_state
 if "count" not in st.session_state:
-  st.session_state("count") = 0
+  st.session_state["count"] = 0
 
