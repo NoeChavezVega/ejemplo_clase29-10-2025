7dc43effc3a0c499d7c7af5a02bb05aecf6b4753
NoeChavezVega <a377347@uach.mx>
2025-10-29 08:40:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 st.title("Ejemplo para usar sesion_state")
 
+count = 0
+
+increment = st.button("Increment")
+if increment :
+  count += 1
+
+
+st.write("Count = , count)
+
+
